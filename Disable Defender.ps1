ad5b04c15a149a94009e7e279ece961a05e69fb2
zoicware <118035521+zoicware@users.noreply.github.com>
2023-07-21 13:31:20 -0400

    Add files via upload

---
+++ Disable Defender.ps1
@@ -0,0 +1,202 @@
+#DISABLE DEFENDER SCRIPT BY ZOIC
+
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) 
+{	Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit	}
+
+    try{
+if((Get-ItemPropertyValue -path registry::HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell -name "ExecutionPolicy") -ne "Bypass" -or (Get-ItemPropertyValue -path registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell -name "ExecutionPolicy") -ne "Bypass"){
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Bypass" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Bypass" /f
+
+
+}
+
+}catch{
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Bypass" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Bypass" /f
+
+}
+
+$uacON = $false
+#disable uac
+if((Get-ItemPropertyValue -path registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -name "EnableLUA") -ne "0"){
+$uacON = $true 
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
+kill -name 'sihost' -force
+}
+#restart explorer
+
+sleep 2
+
+
+Start-Process -FilePath explorer.exe -ArgumentList windowsdefender://threat -WindowStyle Maximized 
+sleep 2
+
+#getting windows security app window state
+
+
+
+pause
+
+
+
+
+$wshell = New-Object -ComObject wscript.shell
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Keyboard
+{
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+}
+"@
+
+# Define key codes
+$VK_ALT = 0x12  # Alt key code
+$VK_SPACE = 0x20  # Space key code
+$VK_X = 0x58  # X key code
+
+# Simulate Alt+Space keystroke combination
+[Keyboard]::keybd_event($VK_ALT, 0, 0, 0)
+[Keyboard]::keybd_event($VK_SPACE, 0, 0, 0)
+Start-Sleep -Milliseconds 100  # Wait for a moment
+[Keyboard]::keybd_event($VK_SPACE, 0, 0x2, 0)
+[Keyboard]::keybd_event($VK_ALT, 0, 0x2, 0)
+
+# Press the 'X' key
+[Keyboard]::keybd_event($VK_X, 0, 0, 0)
+Start-Sleep -Milliseconds 100  # Wait for a moment
+[Keyboard]::keybd_event($VK_X, 0, 0x2, 0)
+
+
+
+sleep 2
+
+
+$wshell.SendKeys("{RIGHT}")
+sleep .65
+$wshell.SendKeys("{DOWN}")
+sleep .65
+$wshell.SendKeys("{DOWN}")
+sleep .65
+$wshell.SendKeys("{DOWN}")
+sleep .65
+$wshell.SendKeys("{DOWN}")
+sleep .65
+$wshell.SendKeys("{LEFT}")
+sleep .65
+$wshell.SendKeys("{UP}")
+sleep .65
+$wshell.SendKeys("{UP}")
+sleep .65
+$wshell.SendKeys("{UP}")
+sleep .65
+$wshell.SendKeys(" ")
+sleep .65
+$wshell.SendKeys(" ")
+
+sleep .75
+
+Stop-Process -name SecHealthUI
+
+
+
+
+$path = "C:\DisableDefender"
+
+if(-Not(Test-Path $path -PathType Leaf)){
+#downloading necessary files
+Write-Host "downloading necessary files..."
+Invoke-RestMethod 'https://github.com/zoicware/Defender/archive/refs/heads/main.zip' -OutFile "C:\Defender.zip"
+Expand-Archive "C:\Defender.zip" -DestinationPath "C:\"
+Remove-Item "C:\Defender.zip"
+Expand-Archive "C:\Defender-main\DisableDefender.zip" -DestinationPath "C:\"
+Remove-Item  "C:\Defender-main" -Force -Recurse
+}
+
+
+
+
+
+#disables defender through gp edit
+Write-Host "Disabling Defender with Group Policy" 
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableSmartScreen" /t REG_DWORD /d "0" /f
+Get-ScheduledTask | Where-Object {$_.Taskname -match 'Windows Defender Cache Maintenance'} | Disable-ScheduledTask
+Get-ScheduledTask | Where-Object {$_.Taskname -match 'Windows Defender Cleanup'} | Disable-ScheduledTask 
+Get-ScheduledTask | Where-Object {$_.Taskname -match 'Windows Defender Scheduled Scan'} | Disable-ScheduledTask
+Get-ScheduledTask | Where-Object {$_.Taskname -match 'Windows Defender Verification'} | Disable-ScheduledTask
+    
+
+#apply gpedit tweaks
+
+gpupdate /force
+
+
+#searching c drive for bat file and power run and then running the bat file with power run
+Write-Host "Disabling Services"
+
+ $defender = Get-ChildItem -Path C:\ -Filter DisableDefend.bat -Recurse -ErrorAction SilentlyContinue -Force |select-object -first 1 | % { $_.FullName; }
+ $nsudo = Get-ChildItem -Path C:\ -Filter NSudoLG.exe -Recurse -ErrorAction SilentlyContinue -Force |select-object -first 1 | % { $_.FullName; }
+       $arguments = "-U:T -P:E -M:S "+"`"$defender`"" 
+        Start-Process -FilePath $nsudo -ArgumentList $arguments -Wait
+
+
+
+
+
+
+
+#clean up files
+Remove-Item "C:\DisableDefender" -Force -Recurse
+
+
+
+[reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null 
+$msgBoxInput = [System.Windows.Forms.MessageBox]::Show('Restart Computer?','zoicware','YesNo','Question')
+
+switch  ($msgBoxInput) {
+
+  'Yes' {
+
+  if($uacON){
+  #setting uac back to default
+  Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "5" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "1" /f
+
+  }
+
+  Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "RemoteSigned" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "RemoteSigned" /f
+
+  #you can guess what this does
+Restart-Computer
+ }
+
+'No'{
+
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "RemoteSigned" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" /f
+Reg.exe add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "RemoteSigned" /f
+
+
+}
+
+}
